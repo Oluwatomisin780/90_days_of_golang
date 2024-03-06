1b25a0bcc8ce444a5cf3c61990f3e3157047b090
oluwatomisin780 <tomisinoyediran@gmail.com>
2024-03-06 11:12:36 +0100

    golang

--- concurrency/DataRace/main.go
+++ concurrency/DataRace/main.go
@@ -13,18 +13,25 @@ func main(){
 	var wg  sync.WaitGroup
 	wg.Add(gr*2)
 	var n int =0 
-
+// mutexes 
+var m sync.Mutex
 	for i:=0;i<gr;i++{
 		go func (){
 			time.Sleep(time.Second/10)
+			m.Lock()
 			n++
+			m.Unlock()
 			wg.Done()
 		}()// function invocation
 
 		go func (){
 			time.Sleep(time.Second/10)
+			m.Lock()
+			defer m.Unlock()
 			n--
+			//m.Unlock()
 			wg.Done()
+
 		}()// function invocation
 	}
 	wg.Wait()
